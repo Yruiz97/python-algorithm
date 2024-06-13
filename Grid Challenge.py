9a059e17558218787cd54b6d873a58d43f1e70b1
Yruiz97 <yulianruizaguirre@gmail.com>
2024-06-13 05:53:48 +0400

    fea: grid challenge

---
+++ Grid Challenge.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    n = len(grid)
+    for i in range(n):
+        grid[i] = sorted(grid[i])
+    # Check columns
+    for col in range(len(grid[0])):
+        col_chars = [grid[row][col] for row in range(n)]
+        if sorted(col_chars) != col_chars:
+            return "NO"
+
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
